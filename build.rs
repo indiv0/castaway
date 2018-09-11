efce0ba4f2d331719f03552263055be8138d9bc0
Nikita Pekin <contact@nikitapek.in>
2018-09-11 12:22:39 -0400

    style: rename a.proto to raft.proto

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use protoc_rust::Customize;
 fn main() {
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/protos",
-        input: &["protos/a.proto"],
+        input: &["protos/raft.proto"],
         includes: &["protos"],
         customize: Customize {
             ..Default::default()
