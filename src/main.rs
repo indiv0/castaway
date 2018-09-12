97460f34008eccd3b2eb45791b9f555eeaaa101e
Nikita Pekin <contact@nikitapek.in>
2018-09-12 16:29:53 -0400

    feat: add Raft RPC server initialization
    Add boilerplate code which initializes a server for the Raft RPC service
    with optional TLS support. Based on boilerplate code from the grpc-examples
    directory in the stepancheg/grpc-rust repo.
    
    Add futures and tls-api-native-tls dependencies.
    Add a `test_tls_acceptor` function which reads a PKCS12 file included at
    compile-time and ensures that the tls-api-native-tls can successfully
    construct a TLS acceptor from this certificate, it then returns the
    built TLS acceptor.
    Add an `is_tls` function to check if the Raft server has been
    initialized with a `--tls` flag, indicating that TLS should be enabled.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+extern crate futures;
 extern crate grpc;
 extern crate protobuf;
 extern crate tls_api;
+extern crate tls_api_native_tls;
 
+use std::env;
+use std::thread;
 use protos::raft::{PingRequest, PingReply};
-use protos::raft_grpc::Raft;
+use protos::raft_grpc::{Raft, RaftServer};
+use tls_api::TlsAcceptorBuilder;
 
 mod protos;
 
@@ -19,6 +24,34 @@ impl Raft for RaftImpl {
     }
 }
 
+fn test_tls_acceptor() -> tls_api_native_tls::TlsAcceptor {
+    let pkcs12 = include_bytes!("../foobar.com.p12");
+    let builder = tls_api_native_tls::TlsAcceptorBuilder::from_pkcs12(pkcs12, "mypass").unwrap();
+    builder.build().unwrap()
+}
+
+fn is_tls() -> bool {
+    env::args().any(|a| a == "--tls")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tls = is_tls();
+
+    let port = if tls { 50052 } else { 50051 };
+
+    let mut server = grpc::ServerBuilder::new();
+    server.http.set_port(port);
+    server.add_service(RaftServer::new_service_def(RaftImpl));
+    server.http.set_cpu_pool_threads(4);
+    if tls {
+        server.http.set_tls(test_tls_acceptor());
+    }
+    let _server = server.build().expect("server");
+
+    println!("raft server started on port {} {}",
+        port, if tls { "with tls" } else { "without tls" });
+
+    loop {
+        thread::park();
+    }
 }
