4c4dcdcf2cc819fe5d2703c53322d18c582cb374
Nikita Pekin <contact@nikitapek.in>
2018-09-12 11:18:19 -0400

    feat: implement the generated Raft trait
    Implement the `Raft` trait generated by protoc-rust-grpc on a bare
    struct (`RaftImpl`). This struct currently only implements the example
    "Raft::SendPing" RPC call, and can be used to initialize an RPC server
    which replies to "Raft::SendPing" RPC calls.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,22 @@ extern crate grpc;
 extern crate protobuf;
 extern crate tls_api;
 
+use protos::raft::{PingRequest, PingReply};
+use protos::raft_grpc::Raft;
+
 mod protos;
+
+struct RaftImpl;
+
+impl Raft for RaftImpl {
+    fn send_ping(&self, _m: grpc::RequestOptions, req: PingRequest) -> grpc::SingleResponse<PingReply> {
+        println!("ping received");
+        let mut r = PingReply::new();
+        let name = if req.get_message().is_empty() { "pong" } else { req.get_message() };
+        r.set_message(name.to_string());
+        grpc::SingleResponse::completed(r)
+    }
+}
 
 fn main() {
     println!("Hello, world!");
