10a35b92049815a08cf77a42d1b3aec93da07e06
Nikita Pekin <contact@nikitapek.in>
2018-11-22 01:00:32 -0500

    chore: remove commented code

--- build.rs
+++ build.rs
@@ -1,13 +1,2 @@
-//extern crate protoc_rust_grpc;
-
 fn main() {
-    /*
-    protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: "src/protos",
-        input: &["protos/raft.proto"],
-        includes: &["protos"],
-        rust_protobuf: true,
-        ..Default::default()
-    }).expect("protoc-rust-grpc");
-    */
 }
